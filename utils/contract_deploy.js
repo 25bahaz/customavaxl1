5f5345d4c3e7457f00d38208c35658e35fdcfd3b
Mehmet Bahadir Dursun <dursunm21@itu.edu.tr>
2025-08-27 09:38:59 +0300

    minor changes

--- utils/contract_deploy.js
+++ utils/contract_deploy.js
@@ -12,9 +12,10 @@ const PRIVATE_KEY = process.env.PRIVATE_KEY;  // prefix yok
 const RPC_URL = process.env.RPC_URL;
 
 const provider = new JsonRpcProvider(RPC_URL);
+const signer = new Wallet(PRIVATE_KEY, provider).connect(provider);
+
 const bytecode = readFileSync("<bin>").toString();
 const abi = JSON.parse(readFileSync("<abi>").toString());
-;
-const signer = new Wallet(PRIVATE_KEY, provider).connect(provider);
+
 const tokenContract = new ContractFactory(abi, bytecode, signer);
 const contract = await tokenContract.deploy();
